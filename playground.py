8f1127c1d3214f15551c58bad0b2ace16eed6404
kevin <kl738@cornell.edu>
2018-11-25 16:09:11 -0500

    try

--- playground.py
+++ playground.py
@@ -3,7 +3,6 @@ Ignore this file. Playground for developer.
 """
 
 import numpy as np
-from scipy.stats import spearmanr
-c = 3
-lst = [1,2,3,4,5,6,7]
-print(lst[len(lst)-c+1:])
+
+lst = [0,1,2,3,4,5]
+print(np.cumsum(lst))
